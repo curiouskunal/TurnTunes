d1a8471794d8ec084182f83dc7ee89d8d3adb73e
Kokul Subendran <kokuls@gmail.com>
2016-01-23 20:29:25 -0500

    Added host page along with placeholders for the player, list, and add song components

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $("#join-btn").click(function() {
  $(".join-input").fadeToggle();
 });
+
+$('.join-input').keypress(function (e) {
+  if (e.which == 13) {
+    dest = $('.join-input').val();
+    window.location.href =  dest + ".html";
+    return false;    
+  }
+});
