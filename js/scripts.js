cdb9344409dce09e62ab9edb9a58dc22cf784aad
Kokul Subendran <kokuls@gmail.com>
2016-01-23 22:26:40 -0500

    Wrote app for mobile first, added responsive media queries for desktops in a separate file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 $("#join-btn").click(function() {
- $(".join-input").fadeToggle();
+
+
+
+
+ if ( $(".join-input").css('visibility') == 'hidden' )
+    $(".join-input").css('visibility','visible').focus();
+  else
+    $(".join-input").css('visibility','hidden');
 });
 
+$(".brand").click(function(){
+  window.location.href =  "index.html";
+})
 $('.join-input').keypress(function (e) {
   if (e.which == 13) {
     dest = $('.join-input').val();
