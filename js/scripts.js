117e1e679ec8f825c021c8e31a675ff4009cba2d
Kokul Subendran <kokuls@gmail.com>
2016-01-28 15:07:41 -0500

    Implemented SoundCloud API
    - Currently in the grape.html page
    - Returns top search result and streams it
    - Need to implement search with multiple results

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
-var usersRef = new Firebase('https://dazzling-torch-8949.firebaseio.com/butter');
+var usersRef = new Firebase('https://dazzling-torch-8949.firebaseio.com/grape');
 var songQueue = new Queue(); // The class was taken from this site http://code.stephenmorley.org/javascript/queues/
-
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
+var playing = false;
+var songCount = 1;
 
 //Firebase data functions
 usersRef.on('child_added', function(childSnapshot) {
-  var song = toTitleCase(childSnapshot.val().song);
+  var key = childSnapshot.key();
+  var song = childSnapshot.val().song;
+  var url = childSnapshot.val().url;
   var votes = childSnapshot.val().votes;
-  //songQueue.enqueue(song);
-
-  $('.playlist').append('<li class="list-group-item"><span class="label label-default label-pill pull-xs-right">'+votes+'</span>'+song+'<i class="upvote fa fa-thumbs-up" align="right"></i</li>');
-  
+  songQueue.enqueue(childSnapshot.val());
+  var length = songQueue.getLength();
+  if (length == 1) {
+  	$("#empty-plist").hide();
+    if (!($('#song-play').attr('src')) || !playing) {
+      playing = true;
+      var newSong = songQueue.dequeue();
+      $('#song-play').attr('src', newSong.url);
+      $(".np-title").text(newSong.song);
+      $(".np-img").attr('src', newSong.img)
+    }
+  }
+  $('.playlist').append('<li class="list-group-item"><span class="label label-default label-pill pull-xs-right">'+songCount+'</span>'+song+'</li>');
+  songCount++;
 });
 
 // Main Page UI functions
@@ -39,18 +47,14 @@ $('.join-input').keypress(function (e) {
 
 $('#host-btn').on('click', function() {
   usersRef.remove();
-  window.location.href =  "butter.html"; //Hardcoded into butter for now
+  window.location.href =  "grape.html"; //Hardcoded into butter for now
   return false;
 });
 
 //Playlist page UI functions
 $('.add-input').bind("enterKey",function(e){
    var song = $(this).val().toLowerCase();
-   usersRef.push({
-     'song': song,
-     'votes': 1
-   });
-   search(song);
+   searchSC(song);
    $(this).val("");
 });
 
@@ -60,27 +64,13 @@ $('.add-input').keyup(function(e){
     }
 });
 
-// function updateVotes(curSong) {
-//   var newSong = true;
-//   usersRef.on("value", function(snapshot) {
-//     snapshot.forEach(function(childSnapshot) {
-//       var key = childSnapshot.key();
-//       var childData = childSnapshot.val();
-//       console.log(childData.song + " = " + curSong);
-//       var num = parseInt(childData.votes) + 1;
-//       console.log(childData.votes + " = " + num);
-//       if (childData.song === curSong) {
-//         console.log("Im in here!");
-//         usersRef.child(key).set({
-//           'song': childData.song,
-//           'votes': parseInt(childData.votes) + 1
-//         });
-//         newSong = false;
-//         return newSong;
-//       }
-//     });
-//     if (!newSong) return newSong;
-//   });
-//   console.log("here?");
-//   return newSong;
-// }
+$('#song-play').on('ended', function() {
+  if (songQueue.peek()) {
+    var newSong = songQueue.dequeue();
+    $(this).attr('src', newSong.url);
+    $(".np-title").text(newSong.song);
+    $(".np-img").attr('src', newSong.img)
+  } else {
+    playing = false;
+  }
+});
---
+++ js/soundcloud.js
@@ -0,0 +1,18 @@
+var clientId = 'e978f526182dc8e9fff2c3802c852c82';
+
+SC.initialize({
+  client_id: clientId
+});
+
+var searchSC = function (query) {
+  SC.get('/tracks', {
+    q: query
+  }).then(function(tracks) {
+    topTrack = tracks[0];
+    usersRef.push({
+      'song': topTrack.title + " - " + topTrack.user.username,
+      'img': topTrack.artwork_url.replace('-large', '-t500x500'),
+      'url': topTrack.stream_url + "?client_id=" + clientId,
+    });
+  });
+};
