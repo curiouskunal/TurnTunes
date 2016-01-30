2f6f46f1538ca5d3580454ff770ac14a16a1af89
Kokul Subendran <kokuls@gmail.com>
2016-01-29 19:36:00 -0500

    added topResults search function, added default cover-art

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -8,12 +8,42 @@ var searchSC = function (query) {
   SC.get('/tracks', {
     q: query
   }).then(function(tracks) {
+    console.log(topResults(tracks));
     topTrack = tracks[0];
     usersRef.push({
       'song': topTrack.title + " - " + topTrack.user.username,
-      'img': topTrack.artwork_url.replace('-large', '-t500x500'),
+      'img': "http://i.imgur.com/665vfkH.png",//topTrack.artwork_url.replace('-large', '-t500x500'),
       'url': topTrack.stream_url + "?client_id=" + clientId,
       'stream?': topTrack.streamable
     });
   });
+};
+
+var topResults = function(results) {
+  var topFive = [];
+  var currentTrack;
+  var artwork;
+
+  for (var i = 0; i < results.length; i++) {
+    currentTrack = results[i];
+
+    if (currentTrack.streamable && topFive.length < 5) {
+      artwork = currentTrack.artwork_url;
+
+      if (artwork == null) {
+        artwork = "http://i.imgur.com/665vfkH.png";
+      } else {
+        artwork = currentTrack.artwork_url.replace('-large', '-t500x500');
+      }
+
+      topFive.push({
+        'song': currentTrack.title + " - " + currentTrack.user.username,
+        'img': artwork,
+        'url': currentTrack.stream_url + "?client_id=" + clientId,
+      })
+    }
+  }
+
+  return topFive;
+
 };
