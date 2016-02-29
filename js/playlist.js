5fed0fabfc1280bf8b45c66c43ea1975d5f824eb
Kokul Subendran <kokuls@gmail.com>
2016-02-29 18:46:04 -0500

    Removed social.js, put share buttons in to playlist.js on document load, storage is now cleared on page exit

--- js/playlist.js
+++ js/playlist.js
@@ -123,6 +123,33 @@ function changeCurrentSong(song) {
 *******************************************************/
 $(document).ready(function() {
   addUser();
+
+  var url = window.location.href;
+  var title = document.title;
+  var text = "Come vibe with me."
+
+  $('.fb').click(function() {
+      window.open('http://www.facebook.com/sharer/sharer.php?u=' + url + '&title=' + title, 'yourWindowName', 'width=600,height=300');
+  });
+
+  $('.tweet').click(function() {
+      window.open('http://twitter.com/intent/tweet?status=' + text + '+' + url, 'yourWindowName', 'width=600,height=250');
+  });
+
+  $('i.copy').tooltip();
+
+  $('.url-share-button').on('click', function() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val("http://turntunes.com/party.html?id=" + partyName).select();
+    document.execCommand("copy");
+    $temp.remove();
+    $('i.copy').attr('title', 'Link Copied!').tooltip('fixTitle').tooltip('show');
+    setTimeout(function() {
+        $('i.copy').attr('title', 'Copy Link').tooltip('fixTitle').tooltip();
+    }, 500);
+  });
+
   if (isHost == 1) {
     $("#song-play").attr('controls', 'controls');
   } else {
@@ -132,6 +159,7 @@ $(document).ready(function() {
 
 window.onunload = function(){
   removeUser();
+  sessionStorage.clear();
 };
 
 $(".brand").click(function () {
